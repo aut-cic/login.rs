2681d0813782f27f9f6bab1479bc1bd4a1d54c1f
Parham Alvani <parham.alvani@gmail.com>
2022-05-18 13:25:17 +0430

    feat: add sample request

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use std::{collections::HashMap, time::Duration};
+
+const USERNAME: &str = "";
+const PASSWORD: &str = "";
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let client = reqwest::blocking::Client::builder()
+        .user_agent("login.rs")
+        .connect_timeout(Duration::from_secs(8))
+        .build()?;
+
+    let req = client
+        .get("http://172.29.1.3:8728/ip/hotspot/active/getall")
+        .basic_auth(USERNAME, Some(PASSWORD))
+        .query(&[("user", "parham.alvani")])
+        .build()?;
+
+    let resp = client.execute(req)?.json::<HashMap<String, String>>()?;
+    println!("{:#?}", resp);
+    Ok(())
 }
